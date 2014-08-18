ff938d734943b63e76935776df7961a53d540a45
Selim <slm.menouar@gmail.com>
2014-08-18 21:06:09 +0200

    add sitemap and meta

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,9 +20,13 @@
 # Learn more: http://github.com/javan/whenever
 
 every 1.day, :at => '5am' do
-  runner "SteamService.udpate_all"
+  runner "SteamService.update_all"
 end
 
 every 1.day, :at => '6am' do
   runner "FirewallService.update!"  
+end
+
+every 1.day, :at => '7:00 am' do
+  rake "-s sitemap:refresh"
 end
