9e8d0df9e57d10865f122cccb79a1154b8a26e99
Selim <slm.menouar@gmail.com>
2014-08-24 23:55:56 +0200

    disable csrf protection on 404 error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  
+  protect_from_forgery with: :exception, except: :render_404
+ 
+
   before_action :set_locale, :set_last_connection
  
 
