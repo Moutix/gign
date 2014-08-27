8fd67e2a154486fdfa0151a7825636a9cbc432e7
Selim <slm.menouar@gmail.com>
2014-08-27 17:17:53 +0200

    add form contact

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,2 +1,17 @@
 class BaseController < ApplicationController
+
+  def mail_contact
+    if !params[:contact][:email].blank? && !params[:contact][:message].blank? && !params[:contact][:name].blank?
+      Mailer.mail_contact(params[:contact][:email], params[:contact][:message], params[:contact][:name], request.remote_ip).deliver
+      flash[:notice] = t("contact.form.success")
+    else
+      flash[:error] = t("contact.form.error")
+    end
+
+    respond_to do |format|
+      format.html{redirect_to contact_path}
+      format.js
+    end
+
+  end
 end
