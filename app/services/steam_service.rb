a29b21e1fc5ea2f50a2d6a07db64ab00685e7232
Selim <slm.menouar@gmail.com>
2016-03-22 21:28:38 +0100

    Correct code styling in steam service

--- app/services/steam_service.rb
+++ app/services/steam_service.rb
@@ -1,5 +1,6 @@
 class SteamService
   attr_accessor :games, :id
+
   def initialize user
     @user = user
     @id = SteamId.new(user.steamid)
@@ -7,42 +8,46 @@ class SteamService
   end
 
   def update!
-    @games.each do |k, game|
-      
+    @games.each_values do |game|
       this_game = find_game(game)
       this_stat = find_stat(this_game)
-      
-      begin 
+
+      begin
         game_stats = @id.game_stats(game.app_id.to_i)
         achievements = game_stats.achievements
       rescue
       end
 
-      if !achievements.nil?
+      unless achievements.nil?
         achievements.each do |achievement|
           find_achievement(achievement, this_game)
         end
       end
 
-      this_stat.update_columns(total_playtime: @id.total_playtime(this_game.app_id), recent_playtime: @id.recent_playtime(this_game.app_id))
+      this_stat.update_columns(total_playtime: @id.total_playtime(this_game.app_id),
+                               recent_playtime: @id.recent_playtime(this_game.app_id))
     end
   end
 
   def short_update!
     update_game!
-    @user.user_stats.where('recent_playtime != 0').where.not(game_id: Game.where(name: @id.most_played_games.keys).pluck(:id)).update_all(recent_playtime: 0)
+    @user.user_stats
+         .where('recent_playtime != 0')
+         .where.not(game_id: Game.where(name: @id.most_played_games.keys).pluck(:id))
+         .update_all(recent_playtime: 0)
+
     update_recent_game!
   end
 
   def update_game!
     (@games.keys - @user.games.pluck(:app_id)).each do |app_id|
       game = find_game(@games[app_id])
-      this_stat = find_stat(game)
+      find_stat(game)
     end
   end
 
   def update_recent_game!
-    @id.most_played_games.each do |k, v|
+    @id.most_played_games.each_key do |k|
       this_game = Game.find_by(name: k)
       return if this_game.nil?
 
@@ -54,30 +59,31 @@ class SteamService
       rescue
       end
 
-      if !achievements.nil?
+      unless achievements.nil?
         achievements.each do |achievement|
           find_achievement(achievement, this_game)
         end
       end
-      
-      this_stat.update_columns(total_playtime: @id.total_playtime(this_game.app_id), recent_playtime: @id.recent_playtime(this_game.app_id))
 
+      this_stat.update_columns(total_playtime: @id.total_playtime(this_game.app_id),
+                               recent_playtime: @id.recent_playtime(this_game.app_id))
     end
   end
 
   def find_game(game)
-    this_game = Game.find_or_create_by(app_id: game.app_id, name: game.name, short_name: game.short_name)
+    this_game = Game.find_or_create_by(app_id: game.app_id,
+                                       name: game.name,
+                                       short_name: game.short_name)
     this_game.update_column(:store_url, game.store_url)
 
     if this_game.images.empty?
       Image.upload_url(game.logo_url, this_game, nil, @user)
     end
-    return this_game
+    this_game
   end
 
   def find_stat(game)
-    this_stat = UserStat.find_or_create_by(user_id: @user.id, game_id: game.id)
-    return this_stat
+    UserStat.find_or_create_by(user_id: @user.id, game_id: game.id)
   end
 
   def find_achievement(achievement, game)
@@ -85,14 +91,20 @@ class SteamService
     name = achievement.name
     name.force_encoding(Encoding::UTF_8)
     begin
-      this_achievement.update_columns(name: name, description: achievement.description, icon_open_url: achievement.icon_open_url, icon_closed_url: achievement.icon_closed_url)
+      this_achievement.update_columns(name: name,
+                                      description: achievement.description,
+                                      icon_open_url: achievement.icon_open_url,
+                                      icon_closed_url: achievement.icon_closed_url)
     rescue
-      p "random error"
+      p 'random error'
     end
+
     if achievement.unlocked?
-      this_user_achievement = UserAchievement.find_or_create_by(user_id: @user.id, achievement_id: this_achievement.id, timestamp: achievement.timestamp)
+      this_user_achievement = UserAchievement.find_or_create_by(user_id: @user.id,
+                                                                achievement_id: this_achievement.id,
+                                                                timestamp: achievement.timestamp)
     end
-    return this_user_achievement
+    this_user_achievement
   end
 
   def update_online_state!
@@ -101,26 +113,28 @@ class SteamService
 
   def self.update_online!
     nb_online = ScanService.scan_steam_players.count
-    SaveData.create(nb_users: User.count, nb_steam_users: User.steam_users.count, nb_online_users: nb_online)
-    return true
+    SaveData.create(nb_users: User.count,
+                    nb_steam_users: User.steam_users.count,
+                    nb_online_users: nb_online)
+    true
   end
 
   def self.update_all!
     start_script = Time.now
-    
+
     puts "Update started at : #{start_script}"
-    
+
     User.public_steam_users.each do |user|
-        steam = self.new(user)
-        ActiveRecord::Base.transaction do
-          user.update_column(:online, steam.id.online?)
-          if user.to_scan
-            steam.update!
-            user.update_columns(to_scan: false)
-          else
-            steam.short_update!
-          end
+      steam = new(user)
+      ActiveRecord::Base.transaction do
+        user.update_column(:online, steam.id.online?)
+        if user.to_scan
+          steam.update!
+          user.update_columns(to_scan: false)
+        else
+          steam.short_update!
         end
+      end
     end
 
     Game.transaction do
@@ -128,17 +142,25 @@ class SteamService
         total_playtime = UserStat.where(game_id: game.id).sum('total_playtime')
         recent_playtime = UserStat.where(game_id: game.id).sum('recent_playtime')
         in_cache = AckbarService.in_cache?(game.app_id)
-        game.update_columns(total_playtime: total_playtime, recent_playtime: recent_playtime, users_count: game.users.length, user_achievements_count: game.user_achievements.length, incache: in_cache)
+
+        game.update_columns(total_playtime: total_playtime,
+                            recent_playtime: recent_playtime,
+                            users_count: game.users.length,
+                            user_achievements_count: game.user_achievements.length,
+                            incache: in_cache)
       end
     end
-    
-    SaveData.create(nb_games: Game.count, nb_played_games: Game.played.count, nb_achievements: UserAchievement.count, recent_playtime: UserStat.all.sum(:recent_playtime), total_playtime: UserStat.all.sum(:total_playtime))
-    
+
+    SaveData.create(nb_games: Game.count,
+                    nb_played_games: Game.played.count,
+                    nb_achievements: UserAchievement.count,
+                    recent_playtime: UserStat.all.sum(:recent_playtime),
+                    total_playtime: UserStat.all.sum(:total_playtime))
+
     end_script = Time.now
 
     puts "Update ended at : #{end_script}"
-    
+
     puts "Executed in #{ActionController::Base.helpers.distance_of_time_in_words(start_script, end_script)}"
   end
-
 end
