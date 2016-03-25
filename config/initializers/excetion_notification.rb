f772d3e1b23febe7ad8a52d890b003787e6f797f
ningirsu <slm.menouar@gmail.com>
2016-03-26 00:11:15 +0100

    Update email in exception notification

--- config/initializers/excetion_notification.rb
+++ config/initializers/excetion_notification.rb
@@ -4,7 +4,7 @@ if defined? ExceptionNotifier
   ExceptionNotification.configure do |config|
     config.add_notifier :email, email_prefix: '[Erreur 500] ',
                                 sender_address: %("Hatsune Miku" <hatsune.miku@larez.fr>),
-                                exception_recipients: %w(selim.menouar@rez-gif.supelec.fr),
+                                exception_recipients: [Rails.configuration.x.gign['contact']],
                                 email_format: :html
   end
 end
